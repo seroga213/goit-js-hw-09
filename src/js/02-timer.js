05103147b3bbfbdb30f79f5f03d7931a2def8c9e
seroga213 <serogaseroga05@gmail.com>
2022-08-29 16:13:34 +0300

    /

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from 'notiflix';
+
 
 const refs = {
   input: document.querySelector('#datetime-picker'),
